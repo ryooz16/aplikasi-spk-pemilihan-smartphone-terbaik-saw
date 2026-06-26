e124726bac281371282c190231a388a8b4a397f0
ryooz16 <riopermanam45@gmail.com>
2026-06-26 15:04:56 +0700

    update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,6 +1,5 @@
 <?php
 $conn = mysqli_connect("localhost", "root", "", "spk_saw");
-// $conn = mysqli_connect("sql311.infinityfree.com", "if0_42274170", "Spkkelompok2", "if0_42274170_spk_saw");
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
